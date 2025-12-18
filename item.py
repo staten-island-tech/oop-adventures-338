431a2ea88845d9bc149813cde14d78a869680452
RL-55 <rickyl55@nycstudents.net>
2025-12-18 09:26:19 -0500

    Create item.py

---
+++ item.py
@@ -0,0 +1,81 @@
+class food:
+    bandage = {
+        "name": "Bandage",
+        "description": "It has already been used several times.",
+        "healing": 10}
+    monstercandy = {
+        "name": "Monster candy",
+        "description": "Has a distinct, non-licorice flavor.",
+        "healing": 10}
+    spiderdonut = {
+        "name": "Spider Donut",
+        "description": "A donut made with Spider Cider in the batter.",
+        "healing": 12}
+    spidercider = {
+        "name": "Spider Cider",
+        "description": "Made with whole spiders, not just the juice.",
+        "healing": 24}
+    butterscotchpie = {
+        "name": "Butterscotch Pie",
+        "description": "Butterscotch-cinnamon pie, one slice.",
+        "healing": 99}
+    snailpie = {
+        "name": "Snail Pie",
+        "description": "Heals Some HP. An acquired taste.",
+        "healing": 99}
+    snowmanpiece = {
+        "name": "Snowman Piece",
+        "description": "Please take this to the ends of the earth.",
+        "healing": 45}
+    nicecream = {
+        "name": "Nice Cream",
+        "description": "Instead of a joke, the wrapper says something nice.",
+        "healing": 15}
+    bisicle = {
+        "name": "Bisicle",
+        "description": "It's a two-pronged popsicle, so you can eat it twice.",
+        "healing": 11}
+    unisicle = {
+        "name": "unisicle",
+        "description": "It's a SINGLE-pronged popsicle. Wait, that's just normal...",
+        "healing": 11}
+    cinnamonbunny = {
+        "name": "Cinnamon Bunny",
+        "description": "A cinnamon roll in the shape of a bunny.",
+        "healing": 22}
+    astronautfood = {
+        "name": "Astronaut Food",
+        "description": "For feeding a pet astronaut.",
+        "healing": 21}
+    crabapple = {
+        "name": "Crab Apple",
+        "description": "An aquatic fruit that resembles a crustacean.",
+        "healing": 18}
+    seatea = {
+        "name": "Sea Tea",
+        "description": "Made from glowing marsh water. Increases SPEED for one battle.",
+        "healing": 10}
+    abandonedquiche = {
+        "name": "Abandoned Quiche",
+        "description": "A psychologically damaged spinach egg pie.",
+        "healing": 34}
+    temmieflakes = {
+        "name": "Temmie Flakes",
+        "description": "It's just torn up pieces of colored construction paper.",
+        "healing": 2}
+    dogsalad = {
+        "name": "Dog Salad",
+        "description": "Recovers HP (Hit Poodles)",
+        "healing": 10}
+    instantnoodles = {
+        "name": "Instant Noodles",
+        "description": "Comes with everything you need for a quick meal!",
+        "healing": 15}
+    hotdog = {
+        "name": "Hot Dog...?",
+        "description": "The meat is made of something called a water sausage.",
+        "healing": 20}
+    hotcat = {
+        "name": "Hot Cat",
+        "description": "Like a hot dog, but with little cat ears on the end.",
+        "healing": 21}
