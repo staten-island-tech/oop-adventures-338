7cfce3d6b540b260817eabbdf91df3a69d40087d
RL-55 <rickyl55@nycstudents.net>
2025-12-22 08:47:47 -0500

    Update item.py

--- item.py
+++ item.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class food:
     bandage = {
         "name": "Bandage",
@@ -79,3 +80,203 @@ class food:
         "name": "Hot Cat",
         "description": "Like a hot dog, but with little cat ears on the end.",
         "healing": 21}
+=======
+food={
+    "bandage" : {
+        "name": "Bandage",
+        "description": "It has already been used several times.",
+        "healing": 10},
+    "monstercandy" : {
+        "name": "Monster candy",
+        "description": "Has a distinct, non-licorice flavor.",
+        "healing": 10},
+    "spiderdonut" : {
+        "name": "Spider Donut",
+        "description": "A donut made with Spider Cider in the batter.",
+        "healing": 12},
+    "spidercider" : {
+        "name": "Spider Cider",
+        "description": "Made with whole spiders, not just the juice.",
+        "healing": 24},
+    "butterscotchpie" : {
+        "name": "Butterscotch Pie",
+        "description": "Butterscotch-cinnamon pie, one slice.",
+        "healing": 99},
+    "snailpie" : {
+        "name": "Snail Pie",
+        "description": "Heals Some HP. An acquired taste.",
+        "healing": 99},
+    "snowmanpiece" : {
+        "name": "Snowman Piece",
+        "description": "Please take this to the ends of the earth.",
+        "healing": 45},
+    "nicecream" : {
+        "name": "Nice Cream",
+        "description": "Instead of a joke, the wrapper says something nice.",
+        "healing": 15},
+    "bisicle" : {
+        "name": "Bisicle",
+        "description": "It's a two-pronged popsicle, so you can eat it twice.",
+        "healing": 11},
+    "unisicle" : {
+        "name": "unisicle",
+        "description": "It's a SINGLE-pronged popsicle. Wait, that's just normal...",
+        "healing": 11},
+    "cinnamonbunny" : {
+        "name": "Cinnamon Bunny",
+        "description": "A cinnamon roll in the shape of a bunny.",
+        "healing": 22},
+    "astronautfood" : {
+        "name": "Astronaut Food",
+        "description": "For feeding a pet astronaut.",
+        "healing": 21},
+    "crabapple" : {
+        "name": "Crab Apple",
+        "description": "An aquatic fruit that resembles a crustacean.",
+        "healing": 18},
+    "seatea" : {
+        "name": "Sea Tea",
+        "description": "Made from glowing marsh water. Increases SPEED for one battle.",
+        "healing": 10},
+    "abandonedquiche" : {
+        "name": "Abandoned Quiche",
+        "description": "A psychologically damaged spinach egg pie.",
+        "healing": 34},
+    "temmieflakes" : {
+        "name": "Temmie Flakes",
+        "description": "It's just torn up pieces of colored construction paper.",
+        "healing": 2},
+    "dogsalad" : {
+        "name": "Dog Salad",
+        "description": "Recovers HP (Hit Poodles)",
+        "healing": 10},
+    "instantnoodles" : {
+        "name": "Instant Noodles",
+        "description": "Comes with everything you need for a quick meal!",
+        "healing": 15},
+    "hotdog" : {
+        "name": "Hot Dog...?",
+        "description": "The meat is made of something called a water sausage.",
+        "healing": 20},
+    "hotcat" : {
+        "name": "Hot Cat",
+        "description": "Like a hot dog, but with little cat ears on the end.",
+        "healing": 21},
+    "junkfood" : {
+        "name": "Junk Food",
+        "description": "Food that was probably once thrown away.",
+        "healing": 17},
+    "hushpuppy" : {
+        "name": "Hush Puppy",
+        "description": "This wonderful spell will stop a dog from casting magic.",
+        "healing": 65},
+    "starfait" : {
+        "name": "Starfait",
+        "description": "A sweet treat made of sparkling stars.",
+        "healing": 14},
+    "glamburger" : {
+        "name": "Glamburger",
+        "description": "A hamburger made of edible glitter and sequins.",
+        "healing": 27},
+    "legendaryhero" : {
+        "name": "Legendary Hero",
+        "description": "Sandwich shaped like a sword. Increases ATTACK when eaten.",
+        "healing": 40},
+    "facesteak" : {
+        "name": "Steak in the Shape of Mettaton's Face",
+        "description": "Huge steak in the shape of Mettaton's face. You don't feel like it's made of real meat...",
+        "healing": 60},
+    "popatochisps" : {
+        "name": "Popato Chisps",
+        "description": "Regular old popato chisps.",
+        "healing": 13},
+    "badmemory" : {
+        "name": "Bad Memory",
+        "description": "?????",
+        "healing": -1},
+    "lastdream" : {
+        "name": "Last Dream",
+        "description": "The goal of Determination.",
+        "healing": 17},
+}
+weapons={
+    "stick" : {
+        "name": "Stick",
+        "description": "Its bark is worse than its bite.",
+        "at": 0},
+    "toyknife" : {
+        "name": "Toy Knife",
+        "description": "Made of plastic. A rarity nowadays.",
+        "at": 3},
+    "toughglove" : {
+        "name": "Tough Glove",
+        "description": "A worn pink leather glove. For five-fingered folk.",
+        "at": 5},
+    "balletshoes" : {
+        "name": "Ballet Shoes",
+        "description": "These used shoes make you feel incredibly dangerous",
+        "at": 7},
+    "tornnotebook" : {
+        "name": "Torn Notebook",
+        "description": "Contains illegible scrawls. Increases INV by 6.",
+        "at": 2},
+    "burntpan" : {
+        "name": "Burnt Pan",
+        "description": "Damage is rather consistent. Consumable items heal four more HP.",
+        "at": 10},
+    "emptygun" : {
+        "name": "Empty Gun",
+        "description": "An antique revolver. It has no ammo. Must be used precisely, or damage will be low.",
+        "at": 12},
+    "worndagger" : {
+        "name": "Worn Dagger",
+        "description": "Perfect for cutting plants and vines.",
+        "at": 15},
+    "realknife" : {
+        "name": "Real Knife",
+        "description": "Here we are!",
+        "at": 999},
+}
+armor={
+    "bandage" : {
+        "name": "Bandage",
+        "description": "It has already been used several times.",
+        "df": 0},
+    "fadedribbon" : {
+        "name": "Faded Ribbon",
+        "description": "If you're cuter, monsters won't hit you as hard.",
+        "df": 3},
+    "manlybandanna" : {
+        "name": "Manly Bandanna",
+        "description": "It has seen some wear. It has abs drawn on it.",
+        "df": 7},
+    "oldtutu" : {
+        "name": "Old Tutu",
+        "description": "Finally, a protective piece of armor.",
+        "df": 10},
+    "cloudyglasses" : {
+        "name": "Cloudy Glasses",
+        "description": "Glasses marred with wear. Increases INV by 9.",
+        "df": 5},
+    "temmiearmor" : {
+        "name": "Temmie Armor",
+        "description": "The things you can do with a college education! Raises ATTACK when worn. Recovers HP every other turn. INV up slightly.",
+        "df": 20},
+    "stainedapron" : {
+        "name": "Stained Apron",
+        "description": "Heals 1 HP every other turn.",
+        "df": 10},
+    "cowboyhat" : {
+        "name": "Cowboy Hat",
+        "description": "This battle-worn hat makes you want to grow a beard. It also raises ATTACK by 5.",
+        "df": 12},
+    "heartlocket" : {
+        "name": "Heart Locket",
+        "description": "It says Best Friends Forever.",
+        "df": 15},
+    "thelocket" : {
+        "name": "The Locket",
+        "description": "You can feel it beating.",
+        "df": 99},
+}
+>>>>>>> Stashed changes
