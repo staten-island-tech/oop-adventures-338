8db4f1dabaa47ed23d7364063e14f7eb86fbbea1
RL-55 <rickyl55@nycstudents.net>
2026-01-06 09:26:31 -0500

    Update game.py

--- game.py
+++ game.py
@@ -3,20 +3,43 @@ from fallen_human import Chara
 from item import food
 from item import weapons
 from item import armor
+from enemy import RuinsEnemies
+from enemy import SnowdinEnemies
+from enemy import WaterfallEnemies
+from enemy import HotlandEnemies
+from enemy import CoreEnemies
+from enemy import NewHomeEnemy
 mysteryman=False
+turn=0
 ruinsencounter=0
 while mysteryman!=True:
     name=(input("Name the fallen human. ")).upper()
     if name=="CHARA":
         print("The TRUE NAME.")
         mysteryman=True
-    if name=="FRISK":
+    elif name=="FRISK":
         print("WARNING: This name will not make your life hell. PROCEED")
         mysteryman=True
-    if name=="GASTER":
-        name=""
-    else:
+    elif name=="SANS":
+        print("nope.")
+        mysteryman=False
+    elif name=="":
+        print("You must choose a name.")
+        mysteryman=False
+    elif name=="PAPYRU" or name=="PAPAYA":
+        print("I'LL ALLOW IT!!!!")
         mysteryman=True
-    
+    elif name=="GASTER":
+        mysteryman=False
+    else:
+       mysteryman=True
 human=Chara(name)
-x=input("check")
+while ruinsencounter<21:
+    random.randint(0,5)
+    enemyalive=True
+    while enemyalive==True:
+        print("")
+        if hp<=0:
+            enemyalive=False
+    ruinsencounter+=1
+
